7393a55aee21467e47ff1751175baa82b07089e8
Nat Welch <nat@natwelch.com>
2013-06-07 23:07:46 -0700

    suspenders

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twomonths::Application.config.secret_token = 'ed27b91972ffbff5817a5bbe9134dfdfbbdc37fd9c50740486b754f0f9bfbe54bdd92497c4a3e42eee2c5c4b26d66fe7159dec2e192d92e061fa84933871c7d5'
---
+++ config/routes.rb
@@ -0,0 +1,2 @@
+Twomonths::Application.routes.draw do
+end
