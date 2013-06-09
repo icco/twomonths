46e358b4aeb5a4c2cfd2e019a49cfad4ca9af63b
Nat Welch <nat@natwelch.com>
2013-06-09 13:04:22 -0700

    use tokens

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Twomonths::Application.config.secret_token = 'ed27b91972ffbff5817a5bbe9134dfdfbbdc37fd9c50740486b754f0f9bfbe54bdd92497c4a3e42eee2c5c4b26d66fe7159dec2e192d92e061fa84933871c7d5'
+Twomonths::Application.config.secret_token = ENV['SECRET_TOKEN'] || 'edasdarlkajdjlsahhj'
