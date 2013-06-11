a832372cd578fe0b5342d9aa0c598161969ef9cd
Nat Welch <nat@natwelch.com>
2013-06-11 15:04:59 -0700

    whee

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,26 @@
 class UserController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+    p params
+  end
+
+  def edit
+
+  end
+
+  def update
+    p params
+  end
+
+  def new
+    404
+  end
+
+  def create
+    404
+  end
 end
