fdd58c9ff000491014c7eb92a5470aa90b583ae0
Nat Welch <nat@natwelch.com>
2013-06-10 21:19:41 -0700

    bringing it over.

--- models/user.rb
+++ models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
+  attr_accessible :email, :telephone, :username
+  validates :username, :presence => true, :format => { :with => /\A[0-9a-zA-Z]+\z/, :message => "Only numbers and letters allowed." }
+  validates :email, :email => true
 
+  def User.find_or_create_from_auth_hash hash
+    username = hash["info"]["username"]
+    if !username.nil?
+      return User.find_or_create_by_username username
+    else
+      return nil
+    end
+  end
 end
