bb3fa9bc31a4e67bec0bb7dc98bb9d95bf2c1929
Nat Welch <nat@natwelch.com>
2013-06-15 13:45:17 -0700

    hopefully a decent twilio cntrlr

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,5 +1,11 @@
 class TwilioController < ApplicationController
   def voice
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say 'Hi. Please record your update.', :voice => 'woman'
+      r.Record
+    end
+
+    render :text => response.text, :content_type => 'text/xml'
   end
 
   def txt
