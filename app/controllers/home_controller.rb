f85484839c13535c1f72afd290bbd993ff7b5933
Nat Welch <nat@natwelch.com>
2013-06-23 13:59:32 -0700

    testing.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   def index
     @user = self.current_user
     if @user
+
+      p @user.updates
+      @user.goals.each {|g| p g.updates }
+
       if @user.goals.empty?
         redirect_to url_for :controller => 'goals', :action => 'new'
       else
