7cefd1ca7a3a78c54fbcf45d739a2c9343f45219
Nat Welch <nat@natwelch.com>
2013-07-03 12:17:18 -0700

    fix all pinging

--- app/models/ping.rb
+++ app/models/ping.rb
@@ -16,14 +16,12 @@ class Ping < ActiveRecord::Base
     when :txt
       client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
       client.account.sms.messages.create(
-        :to => self.user.telephone
-        :body => "Hey there! It's been a while since you updated your goal. How's it going?"
+        :to => self.user.telephone,
+        :body => "Hey there! It's been a while since you updated your goal. How's it going?",
       )
     else
       raise NotImplementedError
     end
-
-    self.save
   end
   handle_asynchronously :deliver
 end
--- lib/tasks/cron.rake
+++ lib/tasks/cron.rake
@@ -1,8 +1,10 @@
-task :cron do
-  User.all.find_each do |user|
-    if (Time.now - user.pings.order("created_at DESC").first.created_at) > 24*60*60
+task :cron => :environment do
+  User.find_each do |user|
+    if user.pings.empty? or ((Time.now - user.pings.order("created_at DESC").first.created_at) > 24*60*60)
       if user.telephone
-        Ping.new(:user => user, :method => :txt).delay.deliver
+        ping = Ping.new(:user => user, :method => :txt)
+        ping.save
+        ping.deliver
       end
     end
   end
