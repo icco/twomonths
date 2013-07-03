3f0f1a1a2e051a1da8dde86bd996dc6f0ac1d1fb
Nat Welch <nat@natwelch.com>
2013-07-03 11:50:02 -0700

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130623161410) do
+ActiveRecord::Schema.define(:version => 20130703183423) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0, :null => false
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(:version => 20130623161410) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+  end
+
+  create_table "pings", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "method"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "updates", :force => true do |t|
