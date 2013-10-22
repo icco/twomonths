1fc3326d78f5b36585f18d29e9d5dccf1207c12c
Nat Welch <nat@natwelch.com>
2013-10-22 01:56:39 -0700

    secret token warning

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Twomonths::Application.config.secret_token = ENV['SECRET_TOKEN'] || '268967f94d036a10a7821393db7693d0ab44762a7fbf7048ca3bd4e4'
+Twomonths::Application.config.secret_key_base = '268967f94d036a10a7821393db7693d0ab44762a7fbf7048ca3bd4e4'
